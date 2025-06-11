ee2934dc4713ba6dac37f76f026ea74e95352510
Joël Favre <git@l3et.com>
2025-06-11 22:16:30 +0200

    Save work

--- apps/tyfo.dev/app/services/folder_service.ts
+++ apps/tyfo.dev/app/services/folder_service.ts
@@ -2,31 +2,55 @@ import { inject } from '@adonisjs/core'
 import Folder from '#models/folder'
 import { FolderRepositoryContract } from '#repositories/contracts/folder_repository_contract'
 import { FolderServiceContract } from '#services/contracts/folder_service_contract'
+import { generateUuid } from '#utils/uuid_helper'
+import { DateTime } from 'luxon'
 
 @inject()
 export default class FolderService implements FolderServiceContract {
   constructor(private folderRepository: FolderRepositoryContract) {}
-  updateFolder(folderUuid: string, data: Partial<Folder>): Promise<Folder> {
-    throw new Error('Method not implemented.')
+  
+  public async updateFolder(folderUuid: string, data: Partial<Folder>): Promise<Folder> {
+    var current = await this.folderRepository.findByUuid(folderUuid);
+    if (!current) {
+      throw new Error(`Folder with UUID ${folderUuid} not found`);
+    }
+    
+    var folder = {
+      uuid: current?.uuid,
+      name: data.name,
+      description: data.description,
+      circleUuid: data.circleId,
+      userUuid: data.userId,
+      createdAt: current?.createdAt,
+      updatedAt: DateTime.now(),
+    }
+    return await this.folderRepository.update(folderUuid, folder);
   }
+
   deleteFolder(folderUuid: string): Promise<void> {
     throw new Error('Method not implemented.')
   }
+
   listFoldersByUser(userUuid: string): Promise<Folder[]> {
     throw new Error('Method not implemented.')
   }
+
   listFoldersByCircle(circleUuid: string): Promise<Folder[]> {
     return this.folderRepository.listByCircleUuid(circleUuid);
   }
+  
   listFoldersByUserAndCircle(userUuid: string, circleUuid: string): Promise<Folder[]> {
     throw new Error('Method not implemented.')
   }
+
   listFoldersByFolderAndUser(folderUuid: string, userUuid: string): Promise<Folder[]> {
     throw new Error('Method not implemented.')
   }
+
   listFoldersByFolderAndCircle(folderUuid: string, circleUuid: string): Promise<Folder[]> {
     throw new Error('Method not implemented.')
   }
+
   listFoldersByFolderAndUserAndCircle(
     folderUuid: string,
     userUuid: string,
@@ -36,7 +60,15 @@ export default class FolderService implements FolderServiceContract {
   }
 
   public async createFolder(data: Partial<Folder>): Promise<Folder> {
-    return await this.folderRepository.create(data)
+    var folder = {
+      uuid: generateUuid(),
+      name: data.name,
+      description: data.description,
+      circleUuid: data.circleId,
+      userUuid: data.userId,
+      createdAt: DateTime.now(),
+    }
+    return await this.folderRepository.create(folder)
   }
 
   public async getFolderByName(name: string): Promise<Folder | null> {
--- apps/tyfo.dev/tests/functional/services/folder_service.spec.ts
+++ apps/tyfo.dev/tests/functional/services/folder_service.spec.ts
@@ -4,6 +4,8 @@ import sinonTest from 'sinon-test';
 
 import FolderService from '#services/folder_service'
 import FolderRepository from '#repositories/folder_repository'
+import Folder from '#models/folder';
+import { create } from 'node:domain';
 
 test.group('FolderService', (group) => {
     let service: FolderService
@@ -16,18 +18,99 @@ test.group('FolderService', (group) => {
     })
 
 
-    test("Test list folder by circle", 
-        stest(function() {
-        var mockService = this.mock(folderRepository);
-        mockService.expects('listByCircleUuid').once().withArgs('12345').returns(Promise.resolve([{
-            uuid: '12345',
-            name: 'Test Folder',
-            description: 'This is a test folder'}]))
+    test("Test list folder by circle",
+        stest(function () {
+            var mockService = this.mock(folderRepository);
+            mockService.expects('listByCircleUuid').once().withArgs('12345').returns(Promise.resolve([{
+                uuid: '12345',
+                name: 'Test Folder',
+                description: 'This is a test folder'
+            }]))
 
             service.listFoldersByCircle('12345');
 
             mockService.verify()
 
-        //assert.isTrue(mockService.verify(), 'listFoldersByCircle should be called once with the correct argument')
-    }))
+            //assert.isTrue(mockService.verify(), 'listFoldersByCircle should be called once with the correct argument')
+        }))
+
+    test("Test create folder",
+        stest(function () {
+
+            var newFolder = new Folder();
+            newFolder.uuid = '12345';
+            newFolder.name = 'Test Folder';
+            newFolder.description = 'This is a test folder';
+
+
+
+            var mockService = this.mock(folderRepository);
+
+            mockService.expects('create').once().withArgs({
+                uuid: sinon.match.any,
+                name: 'Test Folder',
+                description: 'This is a test folder',
+                circleUuid: 1,
+                userUuid: 1,
+                createdAt: sinon.match.any
+            })
+
+            service.createFolder({
+                name: 'Test Folder',
+                description: 'This is a test folder',
+                circleId: 1,
+                userId: 1,
+            })
+
+            mockService.verify()
+
+        }))
+
+    test("Test update folder",
+        stest(function () {
+
+
+            var mockService = this.mock(folderRepository);
+
+            mockService.expects('findByUuid').once().withArgs('12345').returns(
+                Promise.resolve({
+                uuid: '12345',
+                name: 'Old Folder Name',
+                description: 'This is an old folder',
+                circleUuid: 1,
+                userUuid: 1,
+                createdAt: new Date()
+                })
+            )
+
+            mockService.expects('update').once().withArgs({
+                uuid: '12345',
+                name: 'New Folder Name',
+                description: 'This is a new folder',
+                circleUuid: 1,
+                userUuid: 1,
+                createdAt: sinon.match.any,
+                updatedAt: sinon.match.any
+            })
+
+         /*  mockSer.stub(folderRepository, 'findByUuid').resolves({
+                uuid: '12345',
+                name: 'Old Folder Name',
+                description: 'This is an old folder',
+                circleUuid: 1,
+                userUuid: 1,
+                createdAt: new Date()
+                });*/
+
+            service.updateFolder(
+                '12345', {
+                name: 'New Folder Name',
+                description: 'This is a new folder',
+                circleId: 1,
+                userId: 1,
+            })
+
+            mockService.verify()
+
+        }))
 })
